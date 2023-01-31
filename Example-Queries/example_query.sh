8aaf62bfbc03086f9315326a5da188e3529bed44
Nicole Baldy <neb45@uakron.edu>
2023-01-30 20:10:15 -0500

    Initial commit

---
+++ Example-Queries/example_query.sh
@@ -0,0 +1,5 @@
+curl -X POST \
+    -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d @request.json \
+    "https://texttospeech.googleapis.com/v1/text:synthesize"
